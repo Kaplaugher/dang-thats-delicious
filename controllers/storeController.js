fac2575fb7d2293eec2d0aca3da45eacf6c6a4e0
Kaplaugher <kaplaugher94@gmail.com>
2017-05-11 11:26:18 -0400

    Initial commit

---
+++ controllers/storeController.js
@@ -0,0 +1,38 @@
+
+const mongoose = require('mongoose');
+const Store = mongoose.model('Store');
+
+
+exports.homePage = (req,res) => {
+    console.log(req.name);
+    res.render('index');
+}
+
+exports.addStore = (req, res) => {
+    res.render('editStore', { title: 'Add Store' });
+}
+
+exports.createStore = async (req, res) => {
+    const store = await (new Store(req.body)).save();
+    req.flash('success', `Successfully Created ${store.name}. Care to leave a review?`);
+    res.redirect(`/store/${store.slug}`);
+    
+};
+
+exports.getStores = async (req, res) => {
+    // query DB for list of all stores
+    const stores = await Store.find();
+    res.render('stores', { title: 'Stores', stores });
+}
+
+exports.editStore = async (req, res) => {
+    // 1. find store given the id
+    const store = await Store.findOne({_id: req.params.id });
+    res.json(store);
+    // 2. confirm they are the owner of the store
+    // TODO
+    //  3. render out the edit form so the user can update their store
+    res.render('editStore', { title: `Edit ${store.name}`, store});
+    
+}
+
